78f739501952e89c46000274a519ef9dfda4c82d
RanelGM <mr.rr.coon@gmail.com>
2022-01-04 18:17:19 +0300

    Иправляет переход на главную страницу через хлебные крошки

--- src/components/common/breadcrumbs/breadcrumbs.test.tsx
+++ src/components/common/breadcrumbs/breadcrumbs.test.tsx
@@ -1,7 +1,9 @@
 import { Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { createMemoryHistory } from 'history';
 import Breadcumbs from './breadcrumbs';
+import { AppRoute, INITIAL_CATALOG_PAGE } from 'utils/const';
 
 const history = createMemoryHistory();
 
@@ -18,5 +20,19 @@ describe('Breadcrumbs Component', () => {
     expect(screen.getByText(/Главная/i)).toBeInTheDocument();
     expect(screen.getByRole('list')).toBeInTheDocument();
     screen.getAllByRole('link').forEach((link) => expect(link).toBeInTheDocument());
+  });
+
+  it('should redirect to Home Page', () => {
+    render(mockBreadcumbs);
+
+    const linkElement = screen.getByText(/Главная/i);
+    const initialPage = `${AppRoute.Catalog}/${INITIAL_CATALOG_PAGE}`;
+    const homePage = AppRoute.Home;
+
+    history.push(`${AppRoute.Catalog}/${INITIAL_CATALOG_PAGE}`);
+    expect(history.location.pathname).toBe(initialPage);
+
+    userEvent.click(linkElement);
+    expect(history.location.pathname).toBe(homePage);
   });
 });
--- src/components/common/breadcrumbs/breadcrumbs.tsx
+++ src/components/common/breadcrumbs/breadcrumbs.tsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from 'utils/const';
+
 function Breadcrumbs(): JSX.Element {
   return (
     <ul className="breadcrumbs page-content__breadcrumbs">
-      <li className="breadcrumbs__item"><a className="link" href="./main.html">Главная</a>
+      <li className="breadcrumbs__item">
+        <Link to={AppRoute.Home} className="link">Главная</Link>
       </li>
-      <li className="breadcrumbs__item"><a href="#todo" className="link">Каталог</a>
+      <li className="breadcrumbs__item">
+        <a href="#todo" className="link">Каталог</a>
       </li>
     </ul>
   );
