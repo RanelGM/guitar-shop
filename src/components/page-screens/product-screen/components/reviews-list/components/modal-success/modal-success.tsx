e65d27990dce0f86c0e2232a8658519ff09f78f0
RanelGM <mr.rr.coon@gmail.com>
2022-01-20 22:24:42 +0300

    Добавляет фокус лок для модальных окон

--- src/components/page-screens/product-screen/components/reviews-list/components/modal-success/modal-success.tsx
+++ src/components/page-screens/product-screen/components/reviews-list/components/modal-success/modal-success.tsx
@@ -1,4 +1,5 @@
 import { useEffect, MouseEvent } from 'react';
+import FocusLock from 'react-focus-lock';
 import { KeyboardKey } from 'utils/const';
 
 type ModalSuccessProps = {
@@ -35,24 +36,26 @@ function ModalSuccess({ onModalClose }: ModalSuccessProps): JSX.Element {
   };
 
   return (
-    <div className="modal is-active modal--success modal-for-ui-kit">
-      <div className="modal__wrapper">
-        <div onClick={handleOverlayClick} className="modal__overlay"></div>
-        <div className="modal__content">
-          <svg className="modal__icon" width="26" height="20" aria-hidden="true">
-            <use xlinkHref="#icon-success"></use>
-          </svg>
-          <p className="modal__message">Спасибо за ваш отзыв!</p>
-          <div className="modal__button-container modal__button-container--review">
-            <button onClick={handleCloseBtnClick} className="button button--small modal__button modal__button--review">К покупкам!</button>
+    <FocusLock>
+      <div className="modal is-active modal--success modal-for-ui-kit">
+        <div className="modal__wrapper">
+          <div onClick={handleOverlayClick} className="modal__overlay"></div>
+          <div className="modal__content">
+            <svg className="modal__icon" width="26" height="20" aria-hidden="true">
+              <use xlinkHref="#icon-success"></use>
+            </svg>
+            <p className="modal__message">Спасибо за ваш отзыв!</p>
+            <div className="modal__button-container modal__button-container--review">
+              <button onClick={handleCloseBtnClick} className="button button--small modal__button modal__button--review">К покупкам!</button>
+            </div>
+            <button onClick={handleCloseBtnClick} className="modal__close-btn button-cross" type="button" aria-label="Закрыть">
+              <span className="button-cross__icon"></span>
+              <span className="modal__close-btn-interactive-area"></span>
+            </button>
           </div>
-          <button onClick={handleCloseBtnClick} className="modal__close-btn button-cross" type="button" aria-label="Закрыть">
-            <span className="button-cross__icon"></span>
-            <span className="modal__close-btn-interactive-area"></span>
-          </button>
         </div>
       </div>
-    </div>
+    </FocusLock>
   );
 }
 
